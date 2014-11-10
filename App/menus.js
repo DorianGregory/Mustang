20f3353eb2807d51f89d6ed43008707f6dbe8731
Skyscraper2 <gabrielmlundquist@yahoo.com>
2014-11-10 15:44:25 -0500

    Added basic stuff to menus.js [sic]
    - Created canvasWidth and -Height variables to more efficiently
    reference them
    - Created Childs for background and the play button
    - Does other basic functionality stuff

--- App/menus.js
+++ App/menus.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+initiate();
+//creates the scene object
+var scene = new Scene();
+
+//the width and height so we can reference them more efficiently
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+//background Child, playbutton Child (may consider making a Button which inherits from Child)
+var background = new Child(“background.jpg”, 0, 0, canvas.width, canvas.height)
+var playButton = new Child(“playbutton.jpg”, canvasWidth / 2, canvasHeight / 3, canvasWidth / 3,
+                           canvasHeight / 5);
+
+//start the variable with frames per second
+scene.start(60)
+
+//update function that runs with each new frame
+function update(){
+    scene.clear();
+    scene.drawChild(background);
+    scene.drawChild(dude);
+    dude.y++;
+    
+}
