b22fef78241804e12e17bc67c6ec7686658ed14f
Skyscraper2 <gabrielmlundquist@yahoo.com>
2014-11-10 16:00:29 -0500

    Finalized basic Menus code
    - Deleted all that stuff that should be in master
    - Added the onClick method which will do animation and change the state
    once we have it
    - Changed the update function to sceneUpdate function

--- App/menus.js
+++ App/menus.js
@@ -1,25 +1,28 @@
 // JavaScript Document
-initiate();
-//creates the scene object
-var scene = new Scene();
 
 //the width and height so we can reference them more efficiently
 var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 
-//background Child, playbutton Child (may consider making a Button which inherits from Child)
+//background Child
 var background = new Child(“background.jpg”, 0, 0, canvas.width, canvas.height)
-var playButton = new Child(“playbutton.jpg”, canvasWidth / 2, canvasHeight / 3, canvasWidth / 3,
-                           canvasHeight / 5);
 
 //start the variable with frames per second
 scene.start(60)
 
+//play button which goes to start playing
+var playButton = new Child(“playbutton.jpg”, canvasWidth / 2, canvasHeight / 3, canvasWidth / 3,
+                           canvasHeight / 5);
+
+//the thing that activates when you click on it
+playButton.onClick = function(){
+    //here will be animation code but we don't have that yet
+}
+
 //update function that runs with each new frame
-function update(){
+function sceneUpdate(){
     scene.clear();
     scene.drawChild(background);
     scene.drawChild(dude);
     dude.y++;
-    
 }
