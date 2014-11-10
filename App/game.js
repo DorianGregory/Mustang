9a7ada98b7fe0952c8f68c0a14743cb6e8baf0d4
lleontan <lleontan@gmail.com>
2014-11-10 15:13:12 -0500

    hello
    got rid of merge mess

--- App/game.js
+++ App/game.js
@@ -1,11 +1,5 @@
 // JavaScript Document
-<<<<<<< Updated upstream
 // yo bro --Gabriel
 //and Dorian
-<<<<<<< Updated upstream
-=======
 //Neil
->>>>>>> Stashed changes
-=======
 //and leon
->>>>>>> Stashed changes
