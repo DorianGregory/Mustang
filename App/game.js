5e0b99875b320c33ac32650eaaad6a180b068929
DorianGregory <spammeddorian@yahoo.com>
2014-11-10 15:45:30 -0500

    added background and mustang

--- App/game.js
+++ App/game.js
@@ -1,11 +1,5 @@
-// JavaScript Document
-<<<<<<< Updated upstream
-// yo bro --Gabriel
-//and Dorian
-<<<<<<< Updated upstream
-=======
-//Neil
->>>>>>> Stashed changes
-=======
-//and leon
->>>>>>> Stashed changes
+//main character
+var mustang = new Child("Gogh.jpg", 10, 200, 200, 100);
+
+var background = new Child("background.jpg", 0, 0, canvas.width, canvas.height);
+
