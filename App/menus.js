82091da98474fb181aff92932cb178161af865c3
Skyscraper2 <gabrielmlundquist@yahoo.com>
2014-11-24 15:43:44 -0500

    Added menubutton class in menu object
    now we have an all-purpose menu button with a child and an animation
    (idk at least I know how it works)
    also deleted scene.start because ’twas unnecessary

--- App/menus.js
+++ App/menus.js
@@ -4,23 +4,27 @@
 var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 
-//start the variable with frames per second
-scene.start(60)
+//the menu object
+var menu = {};
 
-//the thing that activates when you click on it
-playButton.onClick = function(){
-    startGame();
+//the menubutton class within the menu object
+menu.menuButton = function(buttChild){
+    this.buttonChild = buttChild;
+    this.buttonAnimation;
 }
 
-var menu{};
-
 var startMenu = function(){
+    
     //the background
     menu.background = new Child("background.png", 0, 0, canvas.width, canvas.height);
     
-    //play button which goes to start playing
-    menu.playButton = new Child("playbutton.jpg", canvasWidth / 2, canvasHeight / 3, canvasWidth / 3, canvasHeight / 5);
-
+    //the playbutton, an instantiated menubutton
+    menu.playButton = new menuButton(new Child("playbutton.jpg", canvasWidth / 2, canvasHeight / 3, canvasWidth / 3, canvasHeight / 5));
+    
+    menu.playButton.onClick = function() {
+        startGame();
+    }
+    
 }
 
 //update function that runs with each new frame
