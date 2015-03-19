9143880baf6a16af3bc146ab14de137ae5ce013c
lleontan <lleontan@gmail.com>
2015-03-19 10:02:28 -0400

    moved some code from animation.js to game.js

--- App/animations.js
+++ App/animations.js
@@ -3,16 +3,17 @@
  
  give mustang an array of sprites
  */
-/*
+
  //adds sprites to an object
-function loadSprite(objspritename,filenames,Obj){
+//string,string array, object to add sprites to
+function loadSprites(objSpriteArrayName,filenames,Obj){
     var filelength=filenames.length;
     var arr=[];
     for(var a=0;a<filelength;a++){
         var img=new Image(filenames[a]);
         arr.push(img);
     }
-    Obj.sprites[objspritename]=arr;
+    Obj.sprites[objSpriteArrayName]=arr;
 }
 mustang.animationState="running";       //current state
 mustang.animationCounter=0;             //current frame
@@ -32,37 +33,4 @@ mustang.animate=function(){
 mustang.changeAnimation=function(newState){
     animationCounter=0;
     animationState=newState;
-}*/
-/*mustang.speed=5;        //maximum speed
-mustang.x=50;               //current position
-mustang.y=50;
-mustang.xSpeed=0;           //current speed
-mustang.ySpeed=0;
-mustang.moveX=mustang.x;            //move orders, not currently used so ignore for now.
-mustang.moveY=mustang.y;
-var trackYValues={50,150,200};      //y values of the track
-function updatePosition(movingobj){
-    movingobj.x=movingobj.x+movingobj.xSpeed;
-    movingobj.y=movingobj.y+movingobj.ySpeed;
 }
-function moveHorse(){
-    //lerps the horse to the current track.
-    //The objects new position must be calculated every update.
-    moveTo(mustang.x,trackYValues[mustang.track],mustang);
-    updatePosition(mustang);
-}
-
-function moveTo( newX, newY,moveObj){
-    //sets x and y movespeeds to move to the (x,y) coords, does not mess with rotations
-    
-    var distance=Math.sqrt(Math.pow(newX-moveObj.x,2)+Math.pow(newY-moveObj.y,2));
-    if(distance>3){
-        var xDistance=newX-moveObj.x;
-        var yDistance=newY-moveObj.y;
-        
-        moveObj.xSpeed = (xDistance / (distance / moveObj.speed));
-        moveObj.ySpeed =  (yDistance / (distance / moveObj.speed));
-    }
-    else{
-        moveObj.xSpeed=0;
-        moveObj.ySpeed=0;}*/
--- App/game.js
+++ App/game.js
@@ -65,3 +65,37 @@ function getSpeed(score){
 function transition(from, to){
     
 }
+
+mustang.speed=5;        //maximum speed
+mustang.x=50;               //current position
+mustang.y=50;
+mustang.xSpeed=0;           //current speed
+mustang.ySpeed=0;
+mustang.moveX=mustang.x;            //move orders, not currently used so ignore for now.
+mustang.moveY=mustang.y;
+var trackYValues={50,150,200};      //y values of the track
+function updatePosition(movingobj){
+	movingobj.x=movingobj.x+movingobj.xSpeed;
+	movingobj.y=movingobj.y+movingobj.ySpeed;
+}
+function moveHorse(){
+	//lerps the horse to the current track.
+	//The objects new position must be calculated every update.
+	moveTo(mustang.x,trackYValues[mustang.track],mustang);
+	updatePosition(mustang);
+}
+
+function moveTo( newX, newY,moveObj){
+	//sets x and y movespeeds to move to the (x,y) coords, does not mess with rotations
+	
+	var distance=Math.sqrt(Math.pow(newX-moveObj.x,2)+Math.pow(newY-moveObj.y,2));
+	if(distance>3){
+		var xDistance=newX-moveObj.x;
+		var yDistance=newY-moveObj.y;
+		
+		moveObj.xSpeed = (xDistance / (distance / moveObj.speed));
+		moveObj.ySpeed =  (yDistance / (distance / moveObj.speed));
+	}
+	else{
+		moveObj.xSpeed=0;
+		moveObj.ySpeed=0;}
